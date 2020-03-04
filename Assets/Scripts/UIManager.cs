3b1e5904e033e2c5b52086d60e06defe2040b6b8
jup6039 <42753442+jup6039@users.noreply.github.com>
2020-03-04 13:27:04 -0500

    added functionality: being near a workbench shows prompt to open crafting menu, must be near a workbench to go into crafting menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,12 +7,16 @@ public class UIManager : MonoBehaviour
 {
     public Button modButton;
     public Button craftButton;
+    public GameObject player;
+    public GameObject textPrompt;
     //public Button modButton2;
     //public Button craftButton2;
 
     private GameObject moddingMenu;
     private GameObject craftingMenu;
+    private GameObject[] workbenches;
     private bool isPaused;
+    private bool canCraft;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +24,16 @@ public class UIManager : MonoBehaviour
         Time.timeScale = 1;
         moddingMenu = GameObject.FindGameObjectWithTag("ModdingMenu");
         craftingMenu = GameObject.FindGameObjectWithTag("CraftingMenu");
+        workbenches = GameObject.FindGameObjectsWithTag("Workbench");
+        textPrompt.SetActive(false);
         moddingMenu.SetActive(false);
         craftingMenu.SetActive(false);
         modButton.gameObject.SetActive(false);
         craftButton.gameObject.SetActive(false);
         isPaused = false;
+        canCraft = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -32,13 +41,16 @@ public class UIManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            if (Time.timeScale == 1 && isPaused == false)
+            if (Time.timeScale == 1 && isPaused == false && canCraft == true)
             {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
                 Time.timeScale = 0;
                 isPaused = true;
                 moddingMenu.SetActive(true);
                 modButton.gameObject.SetActive(true);
                 craftButton.gameObject.SetActive(true);
+                textPrompt.SetActive(false);
                 modButton.onClick.AddListener(SwitchToMod);
                 craftButton.onClick.AddListener(SwitchToCraft);
                 //modButton2.onClick.AddListener(SwitchMenu);
@@ -47,11 +59,36 @@ public class UIManager : MonoBehaviour
             }
             else if (Time.timeScale == 0 && isPaused == true)
             {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
                 Debug.Log("high");
                 Time.timeScale = 1;
                 isPaused = false;
                 moddingMenu.SetActive(false);
                 craftingMenu.SetActive(false);
+                modButton.gameObject.SetActive(false);
+                craftButton.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (GameObject bench in workbenches)
+        {
+            /*if ((bench.transform.position.x + 5) <= player.transform.position.x || 
+                (bench.transform.position.x - 5) >= player.transform.position.x || 
+                (bench.transform.position.z + 5) <= player.transform.position.z || 
+                (bench.transform.position.z - 5) >= player.transform.position.z)
+            {
+                textPrompt.SetActive(true);
+            }*/
+            if (bench.GetComponent<BoxCollider>().bounds.Contains(player.transform.position))
+            {
+                textPrompt.SetActive(true);
+                canCraft = true;
+            }
+            else
+            {
+                textPrompt.SetActive(false);
+                canCraft = false;
             }
         }
     }
