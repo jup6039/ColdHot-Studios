3e1d6bfb688da1f6a4aecef25b301a174c8b0a10
jup6039 <jup6039@IGM2000-21>
2020-03-04 18:20:45 -0500

    mod panel basics

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,8 +9,7 @@ public class UIManager : MonoBehaviour
     public Button craftButton;
     public GameObject player;
     public GameObject textPrompt;
-    //public Button modButton2;
-    //public Button craftButton2;
+    public GameObject modPanel;
 
     private GameObject moddingMenu;
     private GameObject craftingMenu;
@@ -18,22 +17,47 @@ public class UIManager : MonoBehaviour
     private bool isPaused;
     private bool canCraft;
 
+    // for testing purposes only
+    //public List<GameObject> mods;
+    public Sprite testMod;
+
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 1;
+
+        // find menu panels
         moddingMenu = GameObject.FindGameObjectWithTag("ModdingMenu");
         craftingMenu = GameObject.FindGameObjectWithTag("CraftingMenu");
         workbenches = GameObject.FindGameObjectsWithTag("Workbench");
+
+        // set all panels and buttons to be invisible
         textPrompt.SetActive(false);
         moddingMenu.SetActive(false);
         craftingMenu.SetActive(false);
         modButton.gameObject.SetActive(false);
         craftButton.gameObject.SetActive(false);
+        modPanel.SetActive(false);
+
+        // bool variables
         isPaused = false;
         canCraft = false;
+
+        // lock cursor for fps
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        // create panel list of all available mods
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject newMod = new GameObject();
+            newMod.transform.SetParent(modPanel.transform);
+            Image newImage = newMod.AddComponent<Image>();
+            newImage.sprite = testMod;
+
+            RectTransform modTransform = newMod.GetComponent<RectTransform>();
+            modTransform.anchoredPosition = new Vector2((100 * -i) - 55, 0);
+        }
     }
 
     // Update is called once per frame
@@ -43,19 +67,24 @@ public class UIManager : MonoBehaviour
         {
             if (Time.timeScale == 1 && isPaused == false && canCraft == true)
             {
+                // unlock cursor
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
+
+                // freeze game
                 Time.timeScale = 0;
+
+                // bool
                 isPaused = true;
+
+                // show menus and set up buttons
                 moddingMenu.SetActive(true);
                 modButton.gameObject.SetActive(true);
                 craftButton.gameObject.SetActive(true);
                 textPrompt.SetActive(false);
                 modButton.onClick.AddListener(SwitchToMod);
                 craftButton.onClick.AddListener(SwitchToCraft);
-                //modButton2.onClick.AddListener(SwitchMenu);
-                //craftButton2.onClick.AddListener(SwitchMenu);
-                //moddingMenu.SetActive(true);
+                modPanel.SetActive(true);
             }
             else if (Time.timeScale == 0 && isPaused == true)
             {
@@ -73,13 +102,6 @@ public class UIManager : MonoBehaviour
 
         foreach (GameObject bench in workbenches)
         {
-            /*if ((bench.transform.position.x + 5) <= player.transform.position.x || 
-                (bench.transform.position.x - 5) >= player.transform.position.x || 
-                (bench.transform.position.z + 5) <= player.transform.position.z || 
-                (bench.transform.position.z - 5) >= player.transform.position.z)
-            {
-                textPrompt.SetActive(true);
-            }*/
             if (bench.GetComponent<BoxCollider>().bounds.Contains(player.transform.position))
             {
                 textPrompt.SetActive(true);
@@ -97,6 +119,7 @@ public class UIManager : MonoBehaviour
     {
         Debug.Log("clicked");
         moddingMenu.SetActive(true);
+        modPanel.SetActive(true);
         craftingMenu.SetActive(false);
     }
 
@@ -104,6 +127,16 @@ public class UIManager : MonoBehaviour
     {
         Debug.Log("clicked");
         moddingMenu.SetActive(false);
+        modPanel.SetActive(false);
         craftingMenu.SetActive(true);
+    }
+
+    // whenever a new mod is created, another mod icon is added to the list
+    // create new gameobject with mod image
+    // look at most recent mod in list
+    // set up anchored position points based on previous mod in list
+    void AddToModList()
+    {
+        
     }
 }
