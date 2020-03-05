488183695e22f1ac9da859474237af55ef453fcb
JLeBerth <42814446+JLeBerth@users.noreply.github.com>
2020-03-04 21:57:54 -0500

    Starting Sprayable Class
    Sprayable class started

---
+++ Assets/Scripts/Interactable Object/Sprayable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprayable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //method that gets called when shot
+    public void Shot()
+    {
+
+    }
+}
